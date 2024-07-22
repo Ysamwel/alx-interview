6e00fc304481be2b81e22e1c59d5ecec69a1a753
ysamwel <yvonneatieno2000@gmail.com>
2024-07-22 18:53:30 +0300

    pascal triangle algorithm in python

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,19 @@
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        #Start of rows
+        row = [1]
+        for j in range(1, i):
+            #sum the two numbers above our current position/row
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            
+        row.append(1)
+
+        triangle.append(row)
+
+    return triangle
