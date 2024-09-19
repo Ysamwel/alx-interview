b00d535c577e08cd5e82040a08f460aae8e2e985
BarakaCaleb <calebbaraka79@gmail.com>
2024-09-19 21:15:17 +0300

    changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Interview Question on: fewest number of coins needed to
+meet a given amount total
+"""
+
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet a given amount total.
+    """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    coin_count = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        coin_count += total // coin
+        total = total % coin
+    if total != 0:
+        return -1
+    return coin_count
