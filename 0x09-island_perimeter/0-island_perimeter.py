1a4fa2ffb848e1a3c3d8aa8fa7bfdccc19f1a7b1
ysamwel <yvonneatieno2000@gmail.com>
2024-09-27 01:18:13 +0300

    grid

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
+"""Island Perimeter Problem
 """
-Function to calculate island perimeter
-"""
+
 
 def island_perimeter(grid):
-    perimeter = 0
-    
+    """
+    Calculates the perimeter of the island described in grid
+    Args:
+        grid: 2d list of integers containing 0(water) or 1(land)
+    Return:
+        the perimeter of the island
+    """
+
+    p = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                perimeter += 4
-
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 1
-                
-                if i < len(grid) - 1 and grid[i + 1][j] == 1:
-                    perimeter -= 1
-                
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 1
-                
-                if j < len(grid[i]) - 1 and grid[i][j + 1] == 1:
-                    perimeter -= 1
-
-    return perimeter
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    p += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    p += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    p += 1
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    p += 1
+    return p
